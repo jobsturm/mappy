a419b06784ee4b7dcbb13d72669cd3ea28ac073c
jobsturm <jobsturm@gmail.com>
2023-05-05 14:40:16 +0200

    Changed to Volar and updating some Typescript issues

--- src/components/hexagon-canvas/HexagonCanvas.vue
+++ src/components/hexagon-canvas/HexagonCanvas.vue
@@ -3,12 +3,14 @@
   import HexagonGrid from './hexagon-classes/HexagonGrid';
   import CameraController from './camera-controller/camera-controller';
   import MouseController from './mouse-controller/mouse-controller';
-  
+  import Toolbar from '../toobar/Toolbar.vue';
+
   const hexagonGrid = new HexagonGrid(window.innerWidth, window.innerHeight);
   const canvas = ref(null);
   const { innerWidth, innerHeight } = window;
 
   onMounted(() => {
+    if (!canvas.value) return;
     new CameraController(canvas.value, hexagonGrid, { width: innerWidth, height: innerHeight });
     new MouseController(canvas.value, hexagonGrid);
     hexagonGrid.setCanvas(canvas.value);
@@ -23,12 +25,21 @@
 <template>
   <div ref="container" class="fill">
     <canvas :width="innerWidth" :height="innerHeight" ref="canvas" class="fill"/>
+    <Toolbar :observables="hexagonGrid.gridNodeObservables" class="main__toolbar"/>
   </div>
 </template>
 
 <style scoped>
-    .fill {
-        width: 100%;
-        height: 100%;
-    }
+  .fill {
+    width: 100%;
+    height: 100%;
+  }
+  .main__toolbar {
+    position: absolute;
+    bottom: 0px;
+    left: 50%;
+    bottom: 24px;
+    transform: translateX(-50%);
+  }
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
   import HexagonCanvas from "../components/hexagon-canvas/HexagonCanvas.vue";
-  import Toolbar from '../components/toobar/Toolbar.vue';
 </script>
 
 <template>
   <main>
     <HexagonCanvas class="main__hexagon-canvas"/>
-    <Toolbar class="main__toolbar"/>
   </main>
 </template>
 
@@ -14,12 +12,5 @@
   main {
     width: 100%;
     height: 100%;
-  }
-  .main__toolbar {
-    position: absolute;
-    bottom: 0px;
-    left: 50%;
-    bottom: 24px;
-    transform: translateX(-50%);
   }
 </style>
