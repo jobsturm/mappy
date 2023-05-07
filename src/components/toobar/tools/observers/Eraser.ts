a28f7e178f46db4b7f0c11bec5aaaf75024a21a3
jobsturm <jobsturm@gmail.com>
2023-05-07 14:15:52 +0200

    Refactored Brush class to painy async because of race condition

--- src/components/toobar/tools/observers/Eraser.ts
+++ src/components/toobar/tools/observers/Eraser.ts
@@ -1,4 +1,4 @@
 import Brush from './base/Brush';
 import { HEX_DEFAULT_COLOR } from '@/components/hexagon-canvas/hexagon-classes/Hexagon';
 
-export default Brush(HEX_DEFAULT_COLOR);
+export default new Brush(HEX_DEFAULT_COLOR);
--- src/components/toobar/tools/observers/PaintBrush.ts
+++ src/components/toobar/tools/observers/PaintBrush.ts
@@ -1,3 +1,3 @@
 import Brush from './base/Brush';
 
-export default Brush('green');
+export default new Brush('green');
